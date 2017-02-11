d6470e8c72872b638b64f7831777faafc94d3511
TT-F <Steven@s-169-232-241-48.resnet.ucla.edu>
2017-02-11 12:12:21 -0800

    微分XcodeVersion1

---
+++ Differentiation/Differentiation/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Differentiation
+//
+//  Created by Steven Shi on 2/11/17.
+//  Copyright © 2017 Liuyi Shi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
